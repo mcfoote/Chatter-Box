9ccad20ea3ce40c91d469f0724b77a7a7d964eb8
Mitchell Foote <mcfoote321@gmail.com>
2023-01-05 20:50:02 -0500

    removes routes to comply with graphql

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { registerUser, authUser } = require("../Controllers/userControllers")
-const router = express.Router()
-
-
-router.route("/").post(registerUser);
-
-
-module.exports = router;
