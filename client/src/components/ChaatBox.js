d69909341dd5d1a82b779ab5f937b30f15aebef7
isain <ibarrais@msu.edu>
2023-01-12 18:23:32 -0500

    progress on chat logic

--- client/src/components/ChaatBox.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ChatBox = () => {
-    return (
-        <div>This is the box containing the message thread</div>
-    );
-};
-
-export default ChatBox;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,36 +1,31 @@
+import { Box, Text } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/Button";
 import React, { useState } from "react";
-import './style/Navbar.css'
+import { useHistory } from "react-router-dom";
+import "./style/Navbar.css";
 
 const Navbar = () => {
-    return (
-        <div className="masterNav">
-            {/* LOGO */}
-            <a className="logo" href='/'></a>
-            {/* NAV */}
+  const history = useHistory();
+  
+  const logoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    history.push("/");
+  };
 
-            <ul className="navbar">
-                <button type="button" id="hamburger">
-                    
-                </button>
-                <li className="noStyle">
-                    <a href="/" className="navItem">Home</a>
-                </li>
-                <li className="noStyle">
-                    <a href="/about" className="navItem">About</a>
-                </li>
-                <li className="noStyle">
-                    <a href="/showcase" className="navItem showcaseLeft">Showcase</a>
-                </li>
-                <li className="noStyle">
-                    {/* link to ACTUAL indeed resume */}
-                    <a href="http://indeed.com" className="navItem">Resume</a>
-                </li>
-                <li className="noStyle">
-                    <a href="/contact" className="navItem">Contact</a>
-                </li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="masterNav">
+      <Box>
+        <h1>Welcome to Chatterbox!</h1>
+        <Button variant="outline">
+          <i class="fa-light fa-message-plus"></i>
+          <Text>New Chat</Text>
+        </Button>
+        <Button variant="outline" onClick={logoutHandler}>
+          Log out
+        </Button>
+      </Box>
+    </div>
+  );
+};
 
 export default Navbar;
