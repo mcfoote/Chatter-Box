5359b01227c3e193792c35233e6ba80a70ce185e
danielc53 <dannyc53@hotmail.com>
2023-01-12 18:49:36 -0500

    repush because why not

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import ChatProvider from "./Context/ChatProvider.js";
+import ChatProvider from "./Context/ChatProvider";
 import "./index.css";
 
 ReactDOM.render(
