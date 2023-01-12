feb153e32a6119a7d413b03851a639a8cfb55da4
isain <ibarrais@msu.edu>
2023-01-11 22:34:49 -0500

    formated MessengerPage, added structure to Homepage.js, and created and gave basic strructure to ChatBox.js/ThreadList.js

--- client/src/Pages/MessengerPage.js
+++ client/src/Pages/MessengerPage.js
@@ -1,12 +1,25 @@
 import Navbar from '../components/Navbar'
-import './style/MessengerPage.css'
+import ThreadList from '../components/ChaatBox';
+import ChatBox from '../components/ChaatBox';
+import './style/MessengerPage.css';
+import { ChatState } from '../Context/ChatProvider';
+import { Box } from "@chakra-ui/layout";
 
 
 const Chatpage = () => {
+  const { user } = ChatState();
 
   return (
     <div style={{ width: "100%" }}>
-      <Navbar />
+      {user && <Navbar />}
+      <Box
+      display="flex"
+      justifyContent="space-between"
+      w="100%"
+      h="90vh">
+        {user && <ThreadList />}
+        {user && <ChatBox />}
+      </Box>
     
       <h1>hello world</h1>
     </div>
