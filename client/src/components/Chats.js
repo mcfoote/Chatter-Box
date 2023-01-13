87439223239a4dc38dab5d4f982e179b374d1e9b
danielc53 <dannyc53@hotmail.com>
2023-01-13 18:55:01 -0500

    set up boneworks chats.js for styling

--- client/src/components/Chats.js
+++ client/src/components/Chats.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Button } from "@chakra-ui/button";
 import { useHistory } from "react-router-dom";
+import Navbar from '../components/Navbar'
+import { Box } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/input";
 
 const Chats = () => {
     const history = useHistory();
@@ -11,10 +14,17 @@ const Chats = () => {
 
     return (
         <div>Thread list of chats
+            <Navbar/>
             <Button onClick={backButton}>Back</Button>
+            <Box>
+                <Box></Box>
+                <div>
+                    <Input></Input>
+                    <Button></Button>
+                </div>
+            </Box>
         </div>
     );
 };
 
 export default Chats;
-
