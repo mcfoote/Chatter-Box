a2f9f5ba6836c5c19b803dbdcb6d44ce610280c6
Mitchell Foote <mcfoote321@gmail.com>
2023-01-13 18:09:17 -0500

    prep for heroku deployment

--- server/server.js
+++ server/server.js
@@ -22,12 +22,17 @@ const server = new ApolloServer({
     context: authMiddleware,
 });
 
+/*
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/src')));
+  }
+  
 
 //serve homepage
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/src/index.html'));
-// })
-
+app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname, '../client/src/index.html'));
+})
+*/
 //function to start server
 const startApolloServer = async (typeDefs, resolvers) => {
 
