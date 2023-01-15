31630132f715b5c0bcccade1874f2dc80b150f03
danielc53 <108687564+danielc53@users.noreply.github.com>
2023-01-15 11:54:31 -0500

    Update Chats.js
    having problems and I have backup if this doesnt work

--- client/src/components/Chats.js
+++ client/src/components/Chats.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@chakra-ui/button";
 import { useHistory } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Box } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/input";
-import "./style/Chats.css";
+import './style/Chats.css'
+import { ADD_MESSAGE } from '../util/mutations'
+import { useMutation } from "@apollo/client";
+import { useToast } from "@chakra-ui/toast";
 
 const Chats = () => {
-  const history = useHistory();
+    const [content, setContent] = useState('');
+    const history = useHistory();
+    const [createMessage] = useMutation(ADD_MESSAGE);
+    // const messageBox = document.getElementsByClassName('messageBox')
+    const toast = useToast();
+
+    const submitHandler = async (e) => {
+        if (!content || content === '' || content === undefined) {
+            toast({
+                title: "Please enter a message",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            return;
+        }
+
+        try {
+            await createMessage({
+                variables: { content }
+            });
+
+        } catch (error) {
+            console.error(error)
+            toast({
+                title: "Error Occured!",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+        }
+    }
 
   const backButton = () => {
     history.push("/messager");
@@ -22,8 +58,16 @@ const Chats = () => {
         <Box className="mainarea">
           <Box className="chatbox"></Box>
           <div className="secondaryarea">
-            <Input className="inputbox"></Input>
-            <Button className="sendbutton">Send</Button>
+            <Input 
+            className="inputbox"
+            placeholder="Start typing..."
+                        name="text"
+                        type='text'/>
+            <Button 
+            onClick={submitHandler} 
+            className="sendbutton">
+                Send
+            </Button>
           </div>
         </Box>
       </div>
