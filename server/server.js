cf1341e7121084a66cf2f0b8e19de400894c5faa
Mitchell Foote <mcfoote321@gmail.com>
2023-01-16 19:20:52 -0500

    working on heroku

--- server/server.js
+++ server/server.js
@@ -24,13 +24,6 @@ const server = new ApolloServer({
 });
 
 
-app.use(express.static('public'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-
-
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
